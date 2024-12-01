3726f4ee21f48bc20e26a3dd69b5ea8e7e62ea6a
RGitJ <rohanjagannathan@outlook.com>
2024-12-01 12:39:24 -0500

    Added event code input and api eventData fetch

--- s3t1solution/src/App.js
+++ s3t1solution/src/App.js
@@ -1,17 +1,47 @@
 
 // Hooks
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Styles
 import './App.css';
 
 const App = () => {
   const [eventStatistics, setEventStatistics] = useState(null);
-  const [eventCode, setEventCode] = useState("");
+  const [eventCode, setEventCode] = useState("2023njski");
+
+  const fetchEvent = async () => {
+    try {
+      const url = `https://api.statbotics.io/v3/event/${eventCode}`;
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const eventData = await response.json();
+      console.log(eventData);
+      return eventData
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const eventData = fetchEvent()
+  }
 
   return (
     <div className="App">
-      
+      <form onSubmit={handleSubmit} className='eventCodeFormGroup'>
+        <input
+          type='text'
+          value={eventCode}
+          onChange={e => setEventCode(e.target.value)}
+          className='eventCodeInput'
+          placeholder='Enter event code...'
+        />
+      </form>
     </div>
   );
 }
