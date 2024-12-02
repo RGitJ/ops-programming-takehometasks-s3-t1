4ebd72da08d06277ed6e3a3d46d574398d0ce5cb
RGitJ <rohanjagannathan@outlook.com>
2024-12-01 23:24:42 -0500

    Updated README for how to run react project & finished EventMatch component

--- s3t1solution/src/components/EventMatch/EventMatch.js
+++ s3t1solution/src/components/EventMatch/EventMatch.js
@@ -12,9 +12,9 @@ const EventMatch = ({ matchData }) => {
 
     return (
         <div className='eventMatch'>
-            <p>{matchData.key}</p>
-            <p>{dateNumToTime(matchData.predicted_time)}</p>
-            <table>
+            <p className='matchTitle'>{matchData.key}</p>
+            <p className='matchTime'>{dateNumToTime(matchData.predicted_time)}</p>
+            <table className='matchStatisticsTable'>
                 <thead>
                     <tr>
                         <th>Alliance</th>
@@ -27,7 +27,7 @@ const EventMatch = ({ matchData }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    <tr bgcolor="skyblue">
                         <td>Red</td>
                         <td>{matchData.red_auto}</td>
                         <td>{matchData.red_teleop}</td>
@@ -36,7 +36,7 @@ const EventMatch = ({ matchData }) => {
                         <td>{matchData.red_score}</td>
                         <td>{matchData.red_endgame_epa_sum + matchData.red_teleop_epa_sum}</td>
                     </tr>
-                    <tr>
+                    <tr bgcolor="crimson">
                         <td>Blue</td>
                         <td>{matchData.blue_auto}</td>
                         <td>{matchData.blue_teleop}</td>
@@ -47,7 +47,27 @@ const EventMatch = ({ matchData }) => {
                     </tr>
                 </tbody>
             </table>
-            
+            <div className='teamsAndVideoContainer'>
+                <table className='teamsTable'>
+                    <tbody>
+                        <tr>
+                            <td bgColor="skyblue">{matchData.blue_1}</td>
+                            <td bgColor="crimson">{matchData.red_1}</td>
+                        </tr>
+                        <tr>
+                            <td bgColor="skyblue">{matchData.blue_2}</td>
+                            <td bgColor="crimson">{matchData.red_2}</td>
+                        </tr>
+                        <tr>
+                            <td bgColor="skyblue">{matchData.blue_3}</td>
+                            <td bgColor="crimson">{matchData.red_3}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                {matchData.video && (
+                    <iframe className="videoEmbed" src={`https://www.youtube.com/embed/${matchData.video}?enablejsapi=1`}></iframe>
+                )}
+            </div>
         </div>
     );
 }
